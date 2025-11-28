34b934b1f4fe001cd925d67eaba8fdf515fa7839
Raimundo Alves de Oliveira <rayalves9322@gmail.com>
2025-11-28 18:38:25 -0300

    Add files via upload

---
+++ dicionarios.py
@@ -0,0 +1,25 @@
+diciplinas = {
+    "Portugues": "Maria vitoria",
+    "Matematica": "João Neto",
+    "Ciencias": "Lucas Guilherme",
+    "Informatica": "Gabriela Alves ",
+    "Medicina": "Marcos Antonio",
+    "Pedagogia": "Roberto Muniz",
+    "Engenharia": "Laura Silva",
+    "Historia": "Luan Pedro",
+    "Geografia": "Adriano Pereira",
+    "Quimica": "Moacir Junior"
+    }
+professor = list(diciplinas.keys())
+
+
+while True:
+    print('  Informe uma dessas materias para ver o nome do professor\n|Portugues,Matematica, Ciencias, Informatica, Medicina, Pedagogia, Engenharia, Historia, Geografia, Quimica ')
+    materia = input(' materia desejada: ').capitalize()
+    if materia in professor: 
+        break
+    else:
+        print('Não foi possivel consultar essa UF tente novamente...')
+
+
+print (f'O professor da materia e: {diciplinas[materia]}')
